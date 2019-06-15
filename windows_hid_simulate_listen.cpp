69a7c07a72139c5d0e367636e95cc04c11294173
Sheprut Ilya <optitel223@gmail.com>
2019-06-15 20:13:37 +0700

    Улучшил программу для работы с обычными клавиатурами * Исправил баг с шифтом * Добавил комментариев * Улучшил структуру кода

--- windows_hid_simulate_listen.cpp
+++ windows_hid_simulate_listen.cpp
@@ -10,7 +10,8 @@
 #include <map>
 
 //-----------------------------------------------------------------------------
-int getInputMethod(void) {
+/** Возвращает айди текущего языка для текущего окна. */
+int getLanguageId(void) {
 	HWND hwnd = GetForegroundWindow();
 	if (hwnd) {
 		DWORD threadID = GetWindowThreadProcessId(hwnd, NULL);
@@ -22,44 +23,91 @@ int getInputMethod(void) {
 }
 
 //-----------------------------------------------------------------------------
+/** Определяет, нажата ли клавиша шифт в данный момент. */
 bool isShiftPressed(void) {
-	return (GetKeyState( VK_SHIFT ) & 0x8000) != 0;
+	return (GetKeyState(VK_SHIFT) & 0x8000) != 0;
 }
 
 //-----------------------------------------------------------------------------
 std::map<int, int> langInputMap;
-int getLanguageId(void) {
-	return langInputMap[getInputMethod()] * 2 + isShiftPressed();
+/** Возвращает номер текущего "слоя", основываясь на нажатом шифте и выбранном языке. Считается, что один язык - это два слоя, первый слой - без шифта, а второй - с шифтом. */
+int getLayer(void) {
+	return langInputMap[getLanguageId()] * 2 + isShiftPressed();
 }
 
 //-----------------------------------------------------------------------------
+/** Инициализирует map с языками и их айди от 0 до количества языков. */
+void initLanguageMap(void) {
+	HKL hh[10];
+	int count = GetKeyboardLayoutList(10, hh);
+	for (int i = 0; i < count; i++)
+		langInputMap[LOWORD(hh[i])] = i;
+}
+
+//-----------------------------------------------------------------------------
+std::map<std::pair<int, int>, bool> pressedMap;
 HHOOK hhook = 0;
 LRESULT __stdcall keyproc( int code, WPARAM wParam, LPARAM lParam ) {
-	/* Начальные присвоения. */
-	int vkCode = ((KBDLLHOOKSTRUCT*) lParam)->vkCode;
+	// Определяем переданные нам данные
+	int vkCode   = ((KBDLLHOOKSTRUCT*) lParam)->vkCode;
 	int scanCode = ((KBDLLHOOKSTRUCT*) lParam)->scanCode;
-	int flags = ((KBDLLHOOKSTRUCT*) lParam)->flags;
+	int flags    = ((KBDLLHOOKSTRUCT*) lParam)->flags;
 	int extraInf = ((KBDLLHOOKSTRUCT*) lParam)->dwExtraInfo;
 
-	std::cout << "KL: col=" << vkCode << ", row=" << isShiftPressed() << ", pressed=" << (wParam == WM_KEYDOWN) << ", layer=" << getLanguageId() << std::endl;
+	bool isPressed = wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN;
+	int layer = getLayer();
 
+	// Костыль. В момент, когда нажимается шифт, нам не надо считать что он нажат, то есть шифт нажимается из четного слоя. Плюс там есть особенности с тем, когда первый раз получается сообщение о нажатом шифте, а функция isShiftPressed() говорит что он не нажат, а потом говорит что нажат.
+	bool isShift = vkCode == VK_RSHIFT || vkCode == VK_LSHIFT || vkCode == VK_SHIFT;
+	if (isShift && isShiftPressed()) {
+		layer--;
+	}
+
+	// Наблюдается такой баг, что при переключении раскладки на Alt+Shift случается так, что откуда-то вылезает клавиша Ctrl с кодом 162, и затем не отпускается. Я не знаю что с этим делать.
+
+	struct Message
+	{
+		int vkCode;
+		int layer;
+		bool isPressed;
+	};
+
+	// Проверяет, отправлялось ли это же сообщение раньше. Функция нужна, чтобы по триста раз не отправлять сообщение "Нажата клавиша N", когда пользователь её зажал и не отпускает.
+	auto isThisMessageAlreadySent = [] (Message msg) {
+		return (msg.isPressed && !pressedMap[{msg.layer, msg.vkCode}]) || !msg.isPressed;
+	};
+
+	// Изменяет свое внутреннее состояние с учетом того, что данное сообщение отправилось. Суть такая, что отправлять подряд несколько сообщений о нажатии клавиши N, нам не надо, а вот когда её отпустили, сообщение о её нажатии можно и отправить.
+	auto notifyThatMessageSent = [] (Message msg) {
+		pressedMap[{msg.layer, msg.vkCode}] = msg.isPressed;
+	};
+
+	// Печатает сообщение на экран.
+	auto printMessage = [] (Message msg) {
+		std::cout << "KL: col=" << msg.vkCode << ", row=" << 0 << ", pressed=" << msg.isPressed << ", layer=" << msg.layer << std::endl;
+	};
+
+	Message msg{vkCode, layer, isPressed};
+	if (isThisMessageAlreadySent(msg)) {
+		printMessage(msg);
+		notifyThatMessageSent(msg);
+	}
+
+	// Так надо делать по документации.
 	return CallNextHookEx(hhook, code, wParam, lParam);
 };
 
 //-----------------------------------------------------------------------------
 int main() {
-	HKL hh[500];
-	int count = GetKeyboardLayoutList(500, hh);
-	for (int i = 0; i < count; i++)
-		langInputMap[LOWORD(hh[i])] = i;
+	initLanguageMap();
 	
-	/* Создание хука и вывод сообщения если он не может быть создан. */
+	// Создание хука или вывод сообщения если он не может быть создан.
 	hhook = SetWindowsHookEx(WH_KEYBOARD_LL, keyproc, NULL, 0);
 	if (NULL == hhook) {
 	    MessageBox(NULL, L"Failed to install hook!", L"Error", MB_ICONERROR);
 	}
 
-	/* Основной цикл работы программы. */
+	// Не знаю почему, но почему-то это работает... А простой вечный цикл нет, хотя по факту тут никакие сообщения не обрабатываются.
 	MSG msg;
 	while(GetMessage(&msg, NULL, 0, 0) > 0)
 	{
